3d271b825d8a72c377c01c56f358f2afaa17c83f
nitsuahlabs@gmail.com <nitsuahlabs@gmail.com>
2025-10-27 17:05:33 -0400

    Refactor code structure for improved readability and maintainability

--- README.md
+++ README.md
@@ -1,14 +1,11 @@
-<div align="center">
-<h1>THREEJS.Multiplayer</h1>
-<img src="https://i.imgur.com/hqehLwp.gif" /><br/>
-<p>React three fiber, socket.io boilerplate multiplayer server and client 💻</p>
-<img src="https://github.com/Nitsuah-Labs/multi/workflows/CI/badge.svg" /><br/>
+# THREEJS.Multiplayer
 
-<a href="#getting-started">Getting started</a>
-</div>
+React three fiber, socket.io boilerplate multiplayer server and client 💻</p>
 
 ## Getting started
+
 To quickly get started
+
 - Fork the repository (`https://github.com/juniorxsound/R3F.Multiplayer`)
 - Clone to local (`git@github.com:Nitsuah-Labs/multi.git`)
 - Install dependencies (Run `npm i` inside the repo root)
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import Spline from '@splinetool/react-spline';
 import Footer from '../components/Footer';
 import "../styles/App.css";
@@ -6,53 +7,57 @@ import "../styles/App.css";
 // CONSTANTS
 const SPLINE_SCENE = `https://prod.spline.design/lwFGUGO5nCfnnDQU/scene.splinecode`;
 
+
 const Home = () => {
+  const navigate = useNavigate();
 
   const renderHome = () => (
-        <div className="container">
-          <div className="projects-container">
-            <div className="projects-card bg-blur">
-              <button className="poly-wallet" onClick={() => window.open("/play","_self")}><h3>PLAY</h3></button>
-              <div>
-                <h4>Free to play Mutliplayer!</h4></div>
-              </div>
-            <div className="projects-card bg-blur">
-              <button className="poly-wallet" onClick={() => window.open("/solo","_self")}><h3>PLAY</h3></button>
-              <div>
-                <h4>Free to play SOLO!</h4></div>
-              </div>
-            <div className="projects-card bg-blur">
-              <button className="poly-wallet" onClick={() => window.open("https://darkmoon.dev","_")}><h3>MARKET</h3></button>
-              <div>
-              <h4>Proceeds will go to opensource software we source.</h4>
-              </div>
-            </div>
-            <div className="projects-card bg-blur">
-              <button className="neutral-wallet"><h3>MINT</h3></button>
-              <div>
-              <p>The Mint contract will fund projects directly.</p>
-              </div>
-            </div>
+    <div className="container">
+      <div className="projects-container">
+        <div className="projects-card bg-blur">
+          <button className="poly-wallet" onClick={() => navigate('/play')}><h3>PLAY</h3></button>
+          <div>
+            <h4>Free to play Mutliplayer!</h4>
+          </div>
+        </div>
+        <div className="projects-card bg-blur">
+          <button className="poly-wallet" onClick={() => navigate('/solo')}><h3>PLAY</h3></button>
+          <div>
+            <h4>Free to play SOLO!</h4>
+          </div>
+        </div>
+        <div className="projects-card bg-blur">
+          <button className="poly-wallet" onClick={() => window.open("https://darkmoon.dev","_blank")}><h3>MARKET</h3></button>
+          <div>
+            <h4>Proceeds will go to opensource software we source.</h4>
+          </div>
+        </div>
+        <div className="projects-card bg-blur">
+          <button className="neutral-wallet"><h3>MINT</h3></button>
+          <div>
+            <p>The Mint contract will fund projects directly.</p>
+          </div>
         </div>
       </div>
+    </div>
   );
 
-return (
-    		<div className="App">
-        <div className="container">
+  return (
+    <div className="App">
+      <div className="container">
         <div className="header">
-            <h2>PLAY🌑DARKMOON</h2>
-          </div>
-            <Spline scene={SPLINE_SCENE} />
-          <div className="middle-row">
-            {renderHome()}
-          </div>
-          <div className="footer-container">
-					  <Footer />
-				  </div>
+          <h2>PLAY🌑DARKMOON</h2>
+        </div>
+        <Spline scene={SPLINE_SCENE} />
+        <div className="middle-row">
+          {renderHome()}
+        </div>
+        <div className="footer-container">
+          <Footer />
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Home
